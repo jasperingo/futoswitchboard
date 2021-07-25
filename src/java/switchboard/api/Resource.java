d514a241eee4d3092407ef24bb7130ad9558d2b3
Anelechukwu <jasperanels@gmail.com>
2021-07-25 08:57:39 +0100

    Made changes to project

--- src/java/switchboard/api/Resource.java
+++ src/java/switchboard/api/Resource.java
@@ -1,32 +1,75 @@
 
 package switchboard.api;
 
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import switchboard.api.response.GetDigits;
+import switchboard.api.response.Play;
+import switchboard.api.response.SResponse;
 
 
 @Path("v1")
+@Produces(MediaType.APPLICATION_XML)
 public class Resource {
-
-    @Context
-    private UriInfo context;
-
     
     public Resource() {
     }
 
    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        return "Yam";
+    @POST
+    @Path("start")
+    public SResponse home(@FormParam("isActive") String isActive) {
+        
+        Play play = new Play();
+        play.setUrl("https://res.cloudinary.com/dvsscfior/video/upload/v1624018988/SWITCHBOARD_bgd1dy.mp3");
+        
+        GetDigits getDigits = new GetDigits();
+        getDigits.setPlay(play);
+        getDigits.setFinishOnKey("#");
+        getDigits.setTimeout(20);
+        getDigits.setCallbackUrl("http://switchboard.iftlab.com.ng:8080/SB/api/v1/route");
+        
+        SResponse response = new SResponse();
+        response.setGetDigits(getDigits);
+        
+        
+        return response;
     }
     
+    
+    @POST
+    @Path("route")
+    public static SResponse router(
+        @FormParam("isActive") String isActive, 
+        @FormParam("sessionId") String sessionId,
+        @FormParam("callerNumber") String callerNum, 
+        @FormParam("callStartTime") String callStartTime,
+        @FormParam("destinationNumber") String dNumber, 
+        @FormParam("dequeuedToPhone") String dequeuedToPhone,
+        @FormParam("direction")String direction, 
+        @FormParam ("amount") String amount,
+        @FormParam("durationinSeconds")String dISecs, 
+        @FormParam("dequeueTime")String dTime,
+        @FormParam("callerCountryCode")String cCCode, 
+        @FormParam("callerCarrierName")String cCN,
+        @FormParam("dtmfDigits") String extCode, 
+        @FormParam("recordingUrl")String recordingUrl
+    ) {
+        
+        
+        
+        
+        return null;
+    }
+    
+    
+    
 }
+
+
 
 
